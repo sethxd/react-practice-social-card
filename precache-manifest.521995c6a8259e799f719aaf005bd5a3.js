d127df13c43899a80ca42735b67ce55dc6ae1c3a
seth dehaan <seth.dehaan@gmail.com>
2019-03-14 09:45:59 -0500

    Updates

---
+++ precache-manifest.521995c6a8259e799f719aaf005bd5a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac598ff9e9a9df051fb8",
+    "url": "/react-practice-social-card/static/css/main.95cb4b35.chunk.css"
+  },
+  {
+    "revision": "ac598ff9e9a9df051fb8",
+    "url": "/react-practice-social-card/static/js/main.b3ff3a41.chunk.js"
+  },
+  {
+    "revision": "572a4a3c33b2b7825250",
+    "url": "/react-practice-social-card/static/js/runtime~main.4c294e7c.js"
+  },
+  {
+    "revision": "21f08c33be360a0481fc",
+    "url": "/react-practice-social-card/static/js/2.90f22486.chunk.js"
+  },
+  {
+    "revision": "cf15d145cfee1cad2ce4b8e5c6a4198a",
+    "url": "/react-practice-social-card/index.html"
+  }
+];
