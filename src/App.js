a5753ea90becb34d1b3d59eea8908c8d82eca8f6
seth dehaan <seth.dehaan@gmail.com>
2019-03-14 09:51:54 -0500

    Published, formatting
    Threw this up on gh pages, mostly to see if I remember how to do it.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SocialCard from './component/Social';
-import data from './component/json-test.json'
+import React, { Component } from "react";
+import "./App.css";
+import SocialCard from "./component/Social";
+import data from "./component/json-test.json";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
           <div className="socialContainer">
-          {data.map((post, key) =>
-            <SocialCard background={post.background} color={post.color} greeting={post.greeting} author={post.author} key={post.greeting} />
-          )}
+            {data.map(post => (
+              <SocialCard
+                background={post.background}
+                color={post.color}
+                greeting={post.greeting}
+                author={post.author}
+              />
+            ))}
           </div>
         </header>
-
       </div>
     );
   }
--- src/component/Social.js
+++ src/component/Social.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
-import logo from '../logo.svg';
-import '../App.css';
+import React, { Component } from "react";
+import "../App.css";
 
 class SocialCard extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            greeting: this.props.greeting,
-            author: this.props.author,
-            background: this.props.background,
-            color:this.props.color,
-            avatar: 'https://api.adorable.io/avatars/150/' + this.props.author
-        }
-    }
-    render() {
-        return (
-
-            <div style={{background: this.state.background, color: this.state.color }} className="socialCard">
-                <img className='logo' src={this.state.avatar} alt='logo' />
-                <div className='greetingContainer'>
-                    <span>{this.state.greeting}</span>
-                </div>
-                <div className="author">
-                    <span>{this.state.author}</span>
-                </div>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      greeting: this.props.greeting,
+      author: this.props.author,
+      background: this.props.background,
+      color: this.props.color,
+      avatar: "https://api.adorable.io/avatars/150/" + this.props.author
+    };
+  }
+  render() {
+    return (
+      <div
+        style={{ background: this.state.background, color: this.state.color }}
+        className="socialCard">
+        <img className="logo" src={this.state.avatar} alt="logo" />
+        <div className="greetingContainer">
+          <span>{this.state.greeting}</span>
+        </div>
+        <div className="author">
+          <span>{this.state.author}</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SocialCard;
